5070768457b2529d7df41149ccbbb455ede5b7cf
Maxime Youyidia <iv_maksim27@MacBook-Air-de-Maxime.local>
2025-11-03 10:26:05 -0500

    Premier envoi du projet sur GitHub

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connexion from './config/connexion';
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ENV = dotenv.config().parsed;
+const PORT = ENV.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur le serveur Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Le serveur depare sur le port ${PORT}`);
+});
